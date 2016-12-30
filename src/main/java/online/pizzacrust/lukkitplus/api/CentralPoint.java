ffdce27ca6ad6ce85f5b279136790f8f97976d84
PizzaCrust <pizzacrustcoder@gmail.com>
2016-12-30 21:21:46 +0700

    Add logger function

--- src/main/java/online/pizzacrust/lukkitplus/api/CentralPoint.java
+++ src/main/java/online/pizzacrust/lukkitplus/api/CentralPoint.java
@@ -1,15 +1,36 @@
 package online.pizzacrust.lukkitplus.api;
 
 
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.Varargs;
+
+import java.util.logging.Logger;
+
+import online.pizzacrust.lukkitplus.environment.FunctionController;
 import online.pizzacrust.lukkitplus.environment.LuaLibrary;
 
 public class CentralPoint extends LuaLibrary.StaticLibrary {
     public CentralPoint() {
-
+        newFunction(new LoggerFunctionController());
     }
+
+    public static Logger LOGGER; // This field will be filled in runtime.
 
     @Override
     public String getName() {
         return "lukkit";
     }
+
+    public static class LoggerFunctionController implements FunctionController {
+        @Override
+        public String getName() {
+            return "logger";
+        }
+
+        @Override
+        public LuaValue onCalled(Varargs parameters) {
+            return new LuaLogger(LOGGER);
+        }
+    }
+
 }
